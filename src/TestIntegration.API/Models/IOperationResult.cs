15d9136f964342f35db89b2ccf289b3cf01de9c7
Shefer Alexander <shefard.aa@gmail.com>
2023-02-17 22:00:31 +0300

    - добавлен класс результата операции

---
+++ src/TestIntegration.API/Models/IOperationResult.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TestIntegration.API.Models
+{
+    /// <summary>
+    /// Результат операции
+    /// </summary>
+    public interface IOperationResult
+    {
+        /// <summary>
+        /// Признак успешности выполнения операции
+        /// </summary>
+        bool IsSuccess { get; }
+
+        /// <summary>
+        /// Ошибки выполнения операции
+        /// </summary>
+        IEnumerable<string> Errors { get; }
+    }
+
+    /// <summary>
+    /// Результат операции
+    /// </summary>
+    public interface IOperationResult<T> : IOperationResult
+    {
+        /// <summary>
+        /// Данные результат операции
+        /// </summary>
+        T Data { get; }
+    }
+}
---
+++ src/TestIntegration.API/Models/OperationResult.cs
@@ -0,0 +1,62 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TestIntegration.API.Models
+{
+    /// <summary>
+    /// Результат операции
+    /// </summary>
+    public record OperationResult : IOperationResult
+    {
+        /// <summary>
+        /// Признак успешности выполнения операции
+        /// </summary>
+        [JsonPropertyName("isSuccess")]
+        public bool IsSuccess => Errors.Count == 0;
+
+        /// <summary>
+        /// Ошибки выполнения операции
+        /// </summary>
+        [JsonPropertyName("errors")]
+        public List<string> Errors { get; init; } = new List<string>();
+
+        IEnumerable<string> IOperationResult.Errors => Errors;
+
+        /// <summary>
+        /// Успешный результат
+        /// </summary>
+        public static OperationResult Successed()
+        {
+            return new OperationResult();
+        }
+
+        /// <summary>
+        /// Неуспешный результат
+        /// </summary>
+        /// <returns></returns>
+        public static OperationResult Failed()
+        {
+            return new OperationResult() { Errors = new List<string>() { "Произошла ошибка при выполнении операции." } };
+        }
+
+        /// <summary>
+        /// Неуспешный результат с указанием причин
+        /// </summary>
+        /// <returns></returns>
+        public static OperationResult Failed(params string[] errors)
+        {
+            return new OperationResult() { Errors = errors.ToList() };
+        }
+    }
+
+    /// <summary>
+    /// Результат операции
+    /// </summary>
+    public record OperationDataResult<T> : OperationResult, IOperationResult<T>
+    {
+        /// <summary>
+        /// Данные результат операции
+        /// </summary>
+        [JsonPropertyName("data")]
+        public T Data { get; init; }
+    }
+}
