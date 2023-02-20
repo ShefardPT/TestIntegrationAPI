6d476110ea588823b2d37304563e4a6ba62b467c
Shefer Alexander <shefard.aa@gmail.com>
2023-02-20 22:07:17 +0300

    - реализован запрос логов запросов данных о пользователях

---
+++ src/TestIntegration.API/Controllers/UsersRequestsLogsController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TestIntegration.API.DataAccess;
+
+namespace TestIntegration.API.Controllers
+{
+    /// <summary>
+    /// Класс-контроллер для получение журнала запросов данных о пользователях
+    /// </summary>
+    [ApiExplorerSettings(IgnoreApi = true)]
+    [Route("api/users/logs")]
+    [ApiController]
+    public class UsersRequestsLogsController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public UsersRequestsLogsController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetLogsAsync(
+            [FromQuery] DateTime? requestedFrom,
+            [FromQuery] DateTime? requestedTo,
+            [FromQuery] string? userLogin)
+        {
+            var query = _appDbContext.UsersDataRequestRecords
+                .AsNoTracking();
+
+            if (requestedFrom.HasValue)
+            {
+                query = query.Where(x => requestedFrom.Value <= x.RequestedOn);
+            }
+
+            if (requestedTo.HasValue)
+            {
+                query = query.Where(x => x.RequestedOn <= requestedTo.Value);
+            }
+
+            if (!string.IsNullOrWhiteSpace(userLogin))
+            {
+                query = query.Where(x => EF.Functions.ILike(x.Login, $"%{userLogin}%"));
+            }
+
+            var logs = await query.ToArrayAsync();
+            
+            return Ok(logs);
+        }
+    }
+}
